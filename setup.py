54e05909dcf9e04a54f1d1668deab833e221645c
rachelegrace <94571546+rachelegrace@users.noreply.github.com>
2022-07-11 10:46:16 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'MyNameIs',
+    version = '1.0.0',
+    url = 'https://github.com/eveschoen/MyNameIs.git',
+    author = 'Eve Schoenrock and Rachel Grace',
+    author_email = 'rg5xm@virginia.edu',
+    description = 'This package takes a name as a string and returns it in an introductory statement.'
+)
+    
